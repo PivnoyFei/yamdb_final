fca34efea79b7b8ed77f7af4db10618245e99f16
Илья Смелов <kekkisama@gmail.com>
2022-09-03 11:42:36 +0300

    Запуск сервера

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default="key")
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.0.173', 'localhost', '*']
+ALLOWED_HOSTS = ['158.160.10.42', 'localhost', '*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
